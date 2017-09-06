e2e11f62688176ba8a996ee9453a8d25ccf103ad
penn <pp.song@yahoo.com>
2017-09-06 15:03:08 +0800

    add heartbeat

--- app.js
+++ app.js
@@ -25,23 +25,27 @@ var io = require('socket.io')(server)
 global.clients = {}
 
 io.on('connection', function (socket) {
-  console.log('user ' + socket.id + ' connected')
-  socket.on('disconnect', function () {
-    console.log('user' + socket.id + ' disconnected')
-    for (key in global.clients) {
-      if (global.clients[key].id == socket.id) {
-        delete global.clients[key]
-        break
-      }
-    }
-    console.log(Object.keys(global.clients))
-  })
+    console.log('user ' + socket.id + ' connected')
+    socket.on('disconnect', function () {
+        console.log('user' + socket.id + ' disconnected')
+        for (key in global.clients) {
+            if (global.clients[key].id == socket.id) {
+                delete global.clients[key]
+                break
+            }
+        }
+        console.log(Object.keys(global.clients))
+    })
 
-  socket.emit('needYourUserId', {need: 'your userId'})
-  socket.on('giveMyUserId', function (userId) {
-    global.clients[userId] = socket
-    console.log(Object.keys(global.clients))
-  })
+    socket.emit('needYourUserId', {need: 'your userId'})
+    socket.on('giveMyUserId', function (userId) {
+        global.clients[userId] = socket
+        console.log(Object.keys(global.clients))
+    })
+
+    socket.on('heartBeat', function (username) {
+        console.log(username)
+    })
 })
 server.listen(3001)
 
@@ -70,19 +74,19 @@ passport.use('local', new LocalStrategy(User.authenticate()))
 
 // Configure Passport to use JWT strategy to look up Users.
 passport.use('jwt', new JwtStrategy(options, function (jwt_payload, done) {
-  console.log('JwtStrategy')
-  User.findOne({
-    _id: jwt_payload.id
-  }, function (err, user) {
-    if (err) {
-      return done(err, false)
-    }
-    if (user) {
-      done(null, user)
-    } else {
-      done(null, false)
-    }
-  })
+    console.log('JwtStrategy')
+    User.findOne({
+        _id: jwt_payload.id
+    }, function (err, user) {
+        if (err) {
+            return done(err, false)
+        }
+        if (user) {
+            done(null, user)
+        } else {
+            done(null, false)
+        }
+    })
 }))
 
 app.use('/', index)
@@ -92,25 +96,25 @@ app.use('/users', users)
 var db = process.env.MONGO_URL || 'localhost/JB3'
 mongoose.connect(db)
 mongoose.connection.on('error', function () {
-  console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?')
+    console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?')
 })
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found')
-  err.status = 404
-  next(err)
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
 })
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+    // render the error page
+    res.status(err.status || 500)
+    res.render('error')
 })
 
 module.exports = app
